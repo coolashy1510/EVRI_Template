2fe4f279e14307ad6cc8adebfe7ab94fc13d0e71
Ashwin Kumar <coolashy1510@gmail.com>
2025-02-05 22:42:33 +0000

    pricing

--- spring-interview-template-master/src/main/java/com/evri/interview/repository/CourierRepository.java
+++
@@ -1,9 +0,0 @@
-package com.evri.interview.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourierRepository extends JpaRepository<CourierEntity, Long> {
-
-}
